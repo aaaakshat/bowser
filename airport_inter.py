b980be0cbe2906dced8dfc5f0bce2f58a100b5c5
aaaakshat <33050725+aaaakshat@users.noreply.github.com>
2021-09-17 22:31:06 -0400

    some problem fix later

--- airport_inter.py
+++ airport_inter.py
@@ -2,6 +2,7 @@
 import subprocess
 import re
 import os
+import time
 
 # Interface with macOS airport util 
 
@@ -9,7 +10,34 @@ AIRPORT_PATH = [os.environ.get(
     'AIRPORT_PATH', 
     '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport')]
 
-def main(args):
-    process = subprocess.run(AIRPORT_PATH + args)
+def gen_cmd(args):
+    return AIRPORT_PATH + args 
 
-main(['-I'])
+def get_info(args):
+    process = subprocess.Popen(gen_cmd(args), stdout=subprocess.PIPE)
+    out, _ = process.communicate()
+    process.wait()
+    return out 
+
+def split_lines(lines, start=0):
+    return lines.split('\n')[start:]
+
+def reformat_info_result(result):
+    def reformat_info_result_line(line):
+        _, value = line.split(':', 1)
+        return value.strip()
+
+    results_in_lines = filter(lambda line: line, split_lines(result))
+    info_args = map(reformat_info_result_line, results_in_lines)
+    return datatypes.InfoResult(*info_args)
+
+def main():
+    while True:
+        a = (get_info(['-I']))
+        print(reformat_info_result(a))
+        time.sleep(1)
+
+
+main()
+
+
